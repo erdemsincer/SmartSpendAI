5865f5016df99830816a7a22cc4a31f42371e38a
Erdem <erdemsincer123@icloud.com>
2025-07-07 23:40:03 +0300

    Add Email value object

---
+++ src/Services/AuthService/AuthService.Domain/ValueObjects/Email.cs
@@ -0,0 +1,18 @@
+﻿namespace AuthService.Domain.ValueObjects;
+
+public record Email
+{
+    public string Value { get; init; }
+
+    private Email(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value) || !value.Contains("@"))
+            throw new ArgumentException("Invalid email address.");
+
+        Value = value.Trim().ToLower();
+    }
+
+    public static Email Create(string email) => new(email);
+
+    public override string ToString() => Value;
+}
