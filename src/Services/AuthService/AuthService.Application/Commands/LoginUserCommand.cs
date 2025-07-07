73358a053762f224bf121eaee8a7a7ef17abcd1b
Erdem <erdemsincer123@icloud.com>
2025-07-07 23:40:02 +0300

    Add LoginUserCommand for user login

---
+++ src/Services/AuthService/AuthService.Application/Commands/LoginUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using AuthService.Application.DTOs;
+
+namespace AuthService.Application.Commands;
+
+public class LoginUserCommand : IRequest<LoginUserResponse>
+{
+    public string Email { get; init; } = null!;
+    public string Password { get; init; } = null!;
+}
