2d2b8a7d27c288e24b57a06e1a2aceec82854054
Erdem <erdemsincer123@icloud.com>
2025-07-07 23:40:02 +0300

    Add validator for RegisterUserCommand

---
+++ src/Services/AuthService/AuthService.Application/Commands/RegisterUserCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace AuthService.Application.Commands;
+
+public class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
+{
+    public RegisterUserCommandValidator()
+    {
+        RuleFor(x => x.FirstName).NotEmpty().MaximumLength(50);
+        RuleFor(x => x.LastName).NotEmpty().MaximumLength(50);
+        RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        RuleFor(x => x.Password).NotEmpty().MinimumLength(6);
+    }
+}
