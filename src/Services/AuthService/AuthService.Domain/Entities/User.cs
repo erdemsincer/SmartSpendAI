397de5033ca1ca5faa2a4c8b1d04d063a0acf1f2
Erdem <erdemsincer123@icloud.com>
2025-07-07 23:40:03 +0300

    Add User entity

---
+++ src/Services/AuthService/AuthService.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using AuthService.Domain.Enums;
+
+namespace AuthService.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+
+    public Role Role { get; set; } = Role.User;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
