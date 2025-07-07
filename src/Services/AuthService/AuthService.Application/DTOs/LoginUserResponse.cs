c4eaa14ae6c71df37421f8fb2af3d11f62debf0e
Erdem <erdemsincer123@icloud.com>
2025-07-07 23:40:03 +0300

    Add DTO for login user response

---
+++ src/Services/AuthService/AuthService.Application/DTOs/LoginUserResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthService.Application.DTOs;
+
+public class LoginUserResponse
+{
+    public string AccessToken { get; set; } = null!;
+    public Guid UserId { get; set; }
+    public string Email { get; set; } = null!;
+    public string FullName { get; set; } = null!;
+    public string Role { get; set; } = null!;
+}
