959049323be5c261a2362d403415a5a75e255b8e
Erdem <erdemsincer123@icloud.com>
2025-07-07 23:40:02 +0300

    Add RegisterUserCommand for user registration

---
+++ src/Services/AuthService/AuthService.Application/Commands/RegisterUserCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace AuthService.Application.Commands;
+
+public class RegisterUserCommand : IRequest<Guid>  
+{
+    public string FirstName { get; init; } = null!;
+    public string LastName { get; init; } = null!;
+    public string Email { get; init; } = null!;
+    public string Password { get; init; } = null!;
+}
