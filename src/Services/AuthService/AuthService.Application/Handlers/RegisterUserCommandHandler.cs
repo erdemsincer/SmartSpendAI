c6b5692482afde9e81e88089549d2af077dd4ca7
Erdem <erdemsincer123@icloud.com>
2025-07-07 23:40:03 +0300

    Add handler for register command

---
+++ src/Services/AuthService/AuthService.Application/Handlers/RegisterUserCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using AuthService.Application.Commands;
+using AuthService.Domain.Entities;
+using AuthService.Domain.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthService.Application.Handlers;
+
+public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, Guid>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IPasswordHasher<User> _passwordHasher;
+
+    public RegisterUserCommandHandler(IUserRepository userRepository, IPasswordHasher<User> passwordHasher)
+    {
+        _userRepository = userRepository;
+        _passwordHasher = passwordHasher;
+    }
+
+    public async Task<Guid> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+    {
+        if (await _userRepository.EmailExistsAsync(request.Email))
+            throw new InvalidOperationException("Email is already registered.");
+
+        var user = new User
+        {
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Email = request.Email,
+            PasswordHash = "" // Hash birazdan
+        };
+
+        user.PasswordHash = _passwordHasher.HashPassword(user, request.Password);
+
+        await _userRepository.AddAsync(user);
+
+        return user.Id;
+    }
+}
