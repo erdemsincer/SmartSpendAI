608d95a5fcba410df2280b5077f61966e33afa12
Erdem <erdemsincer123@icloud.com>
2025-07-07 23:40:04 +0300

    Add AuthDbContext for EF Core

---
+++ src/Services/AuthService/AuthService.Infrastructure/Persistence/AuthDbContext.cs
@@ -0,0 +1,28 @@
+﻿using AuthService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace AuthService.Infrastructure.Persistence;
+
+public class AuthDbContext : DbContext
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(u => u.Id);
+            entity.HasIndex(u => u.Email).IsUnique();
+            entity.Property(u => u.FirstName).IsRequired().HasMaxLength(50);
+            entity.Property(u => u.LastName).IsRequired().HasMaxLength(50);
+            entity.Property(u => u.Email).IsRequired().HasMaxLength(100);
+            entity.Property(u => u.PasswordHash).IsRequired();
+        });
+    }
+}
