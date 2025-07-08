5c888e1b3b7d5f8273caf7729720dda04d7ac5b5
Erdem <erdemsincer123@icloud.com>
2025-07-08 21:58:27 +0300

    Add EF OCRDbContext

---
+++ src/Services/OCRService/OCRService.Infrastructure/Persistence/OCRDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OCRService.Domain.Entities;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace OCRService.Infrastructure.Persistence;
+
+public class OCRDbContext : DbContext
+{
+    public OCRDbContext(DbContextOptions<OCRDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<OcrResult> OcrResults => Set<OcrResult>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<OcrResult>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.RawText).IsRequired();
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
