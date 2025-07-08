b4cc14df975e73e69ed7de04348dda1aeb7c0680
Erdem <erdemsincer123@icloud.com>
2025-07-08 21:58:27 +0300

    Add ServiceRegistration for OCRService DI setup

---
+++ src/Services/OCRService/OCRService.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OCRService.Domain.Interfaces;
+using OCRService.Infrastructure.Persistence;
+using OCRService.Infrastructure.Repositories;
+
+namespace OCRService.Infrastructure;
+
+public static class ServiceRegistration
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<OCRDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IOcrResultRepository, OcrResultRepository>();
+
+        return services;
+    }
+}
