d93be87aa181b6eb9bcbeb0c43b22e1e4a950250
Erdem <erdemsincer123@icloud.com>
2025-07-08 20:58:20 +0300

    Add Tesseract OCR processor implementation

---
+++ src/Services/OCRService/OCRService.Worker/Processors/TesseractOcrProcessor.cs
@@ -0,0 +1,18 @@
+﻿using OCRService.Worker.Interfaces;
+using Tesseract;
+
+namespace OCRService.Worker.Processors;
+
+public class TesseractOcrProcessor : IOcrProcessor
+{
+    public async Task<string> ProcessImageAsync(string filePath)
+    {
+        return await Task.Run(() =>
+        {
+            using var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default);
+            using var img = Pix.LoadFromFile(filePath);
+            using var page = engine.Process(img);
+            return page.GetText();
+        });
+    }
+}
