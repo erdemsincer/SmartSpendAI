8561b2e17b9effe4625a4231693edf7b35770c33
Erdem <erdemsincer123@icloud.com>
2025-07-08 14:34:12 +0300

    Add handler for processing UploadReceiptCommand

---
+++ src/Services/UploadService/UploadService.Application/Handlers/UploadReceiptCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using UploadService.Application.Commands;
+using UploadService.Domain.Entities;
+using UploadService.Domain.Enums;
+using UploadService.Domain.Interfaces;
+
+namespace UploadService.Application.Handlers;
+
+public class UploadReceiptCommandHandler : IRequestHandler<UploadReceiptCommand, Guid>
+{
+    private readonly IReceiptRepository _repository;
+
+    public UploadReceiptCommandHandler(IReceiptRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Guid> Handle(UploadReceiptCommand request, CancellationToken cancellationToken)
+    {
+        var file = new ReceiptFile
+        {
+            UserId = request.UserId,
+            FileName = request.FileName,
+            FilePath = request.FilePath,
+            FileType = (FileType)request.FileType,
+            UploadedAt = DateTime.UtcNow
+        };
+
+        await _repository.AddAsync(file);
+
+        // TODO: Raise OCR event (RabbitMQ veya EventBus ile)
+
+        return file.Id;
+    }
+}
