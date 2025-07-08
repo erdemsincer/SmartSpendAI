e57385be3ee7b7fd5ee6649a935aa372c8ca27f9
Erdem <erdemsincer123@icloud.com>
2025-07-08 21:58:22 +0300

    Add OcrResult entity to domain layer

---
+++ src/Services/OCRService/OCRService.Domain/Entities/OcrResult.cs
@@ -0,0 +1,14 @@
+﻿namespace OCRService.Domain.Entities;
+
+public class OcrResult
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public Guid FileId { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public string RawText { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
