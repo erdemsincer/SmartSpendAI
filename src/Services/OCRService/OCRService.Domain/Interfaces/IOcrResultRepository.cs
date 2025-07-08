40e7452454e2085cb99c50e6300d213b9a66991e
Erdem <erdemsincer123@icloud.com>
2025-07-08 21:58:22 +0300

    Add IOcrResultRepository interface

---
+++ src/Services/OCRService/OCRService.Domain/Interfaces/IOcrResultRepository.cs
@@ -0,0 +1,9 @@
+﻿using OCRService.Domain.Entities;
+
+namespace OCRService.Domain.Interfaces;
+
+public interface IOcrResultRepository
+{
+    Task AddAsync(OcrResult result);
+    Task<OcrResult?> GetByFileIdAsync(Guid fileId);
+}
