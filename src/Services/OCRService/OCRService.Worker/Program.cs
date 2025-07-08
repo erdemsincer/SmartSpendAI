a83fbf924ec8ab4746b17a2f297d036a4aa3a2be
Erdem <erdemsincer123@icloud.com>
2025-07-08 21:58:37 +0300

    Update Program.cs for OCR background worker

--- src/Services/OCRService/OCRService.Worker/Program.cs
+++ src/Services/OCRService/OCRService.Worker/Program.cs
@@ -1,14 +1,28 @@
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.EntityFrameworkCore;
+using OCRService.Infrastructure;
+using OCRService.Infrastructure.Persistence;
 using OCRService.Worker.Interfaces;
+using OCRService.Worker.Messaging;
 using OCRService.Worker.Processors;
 using OCRService.Worker.Services;
 
-Host.CreateDefaultBuilder(args)
+var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) =>
     {
         services.AddSingleton<IOcrProcessor, TesseractOcrProcessor>();
         services.AddHostedService<OcrWorker>();
+
+        services.AddInfrastructure(context.Configuration); // DI + DbContext burada
+        services.AddSingleton<IEventPublisher, RabbitMqEventPublisher>();
+
     })
-    .Build()
-    .Run();
+    .Build();
+
+// 🚀 Otomatik Migration
+using (var scope = host.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<OCRDbContext>();
+    db.Database.Migrate();
+}
+
+host.Run();
