c559d9a4dff03dfffba82acaf830784307a434b8
Erdem <erdemsincer123@icloud.com>
2025-07-08 14:34:18 +0300

    Add IReceiptRepository interface for abstraction

---
+++ src/Services/UploadService/UploadService.Domain/Interfaces/IReceiptRepository.cs
@@ -0,0 +1,11 @@
+﻿using UploadService.Domain.Entities;
+
+namespace UploadService.Domain.Interfaces;
+
+public interface IReceiptRepository
+{
+    Task AddAsync(ReceiptFile file);
+    Task<ReceiptFile?> GetByIdAsync(Guid id);
+    Task<IEnumerable<ReceiptFile>> GetByUserIdAsync(Guid userId);
+    Task UpdateAsync(ReceiptFile file);  // isProcessed flag'i için
+}
