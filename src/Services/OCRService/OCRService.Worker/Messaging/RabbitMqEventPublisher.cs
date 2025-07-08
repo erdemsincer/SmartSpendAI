388b9d2dbcf17260e4f6ab4f90dbd0004e0818a0
Erdem <erdemsincer123@icloud.com>
2025-07-08 21:58:37 +0300

    Add RabbitMQ publisher to worker messaging

---
+++ src/Services/OCRService/OCRService.Worker/Messaging/RabbitMqEventPublisher.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Text.Json;
+using OCRService.Worker.Interfaces;
+using RabbitMQ.Client;
+
+namespace OCRService.Worker.Messaging;
+
+public class RabbitMqEventPublisher : IEventPublisher
+{
+    private readonly IConnection _connection;
+
+    public RabbitMqEventPublisher()
+    {
+        var factory = new ConnectionFactory() { HostName = "rabbitmq", Port = 5672 };
+        _connection = factory.CreateConnection();
+    }
+
+    public Task PublishAsync<T>(T @event, string queueName)
+    {
+        using var channel = _connection.CreateModel();
+        channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false);
+
+        var json = JsonSerializer.Serialize(@event);
+        var body = Encoding.UTF8.GetBytes(json);
+
+        channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: null, body: body);
+        return Task.CompletedTask;
+    }
+}
