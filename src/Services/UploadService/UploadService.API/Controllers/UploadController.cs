9a4f7f8bd52696ad37385cc127d8fdf01508a590
Erdem <erdemsincer123@icloud.com>
2025-07-08 14:34:03 +0300

    Add UploadController for handling receipt upload requests

---
+++ src/Services/UploadService/UploadService.API/Controllers/UploadController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using UploadService.Application.Commands;
+
+namespace UploadService.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UploadController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public UploadController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Upload([FromBody] UploadReceiptCommand command)
+    {
+        var id = await _mediator.Send(command);
+        return Ok(new { receiptId = id });
+    }
+}
