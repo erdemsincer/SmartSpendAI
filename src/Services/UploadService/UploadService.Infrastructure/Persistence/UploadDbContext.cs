b102ac94cd640126e31500bbc0c60a59424e5874
Erdem <erdemsincer123@icloud.com>
2025-07-08 14:34:26 +0300

    Add UploadDbContext for EF Core

---
+++ src/Services/UploadService/UploadService.Infrastructure/Persistence/UploadDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UploadService.Domain.Entities;
+
+namespace UploadService.Infrastructure.Persistence;
+
+public class UploadDbContext : DbContext
+{
+    public UploadDbContext(DbContextOptions<UploadDbContext> options) : base(options) { }
+
+    public DbSet<ReceiptFile> ReceiptFiles => Set<ReceiptFile>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ReceiptFile>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.FileName).IsRequired().HasMaxLength(255);
+            entity.Property(x => x.FilePath).IsRequired();
+            entity.Property(x => x.UserId).IsRequired();
+            entity.Property(x => x.FileType).IsRequired();
+            entity.Property(x => x.IsProcessed).IsRequired();
+        });
+    }
+}
