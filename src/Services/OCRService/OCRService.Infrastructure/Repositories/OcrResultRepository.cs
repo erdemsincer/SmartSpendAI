d7da7b23694d26459b9091a22d9f3fe3e432f025
Erdem <erdemsincer123@icloud.com>
2025-07-08 21:58:27 +0300

    Add OcrResultRepository implementation

---
+++ src/Services/OCRService/OCRService.Infrastructure/Repositories/OcrResultRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OCRService.Domain.Entities;
+using OCRService.Domain.Interfaces;
+using OCRService.Infrastructure.Persistence;
+
+namespace OCRService.Infrastructure.Repositories;
+
+public class OcrResultRepository : IOcrResultRepository
+{
+    private readonly OCRDbContext _context;
+
+    public OcrResultRepository(OCRDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(OcrResult result)
+    {
+        _context.OcrResults.Add(result);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<OcrResult?> GetByFileIdAsync(Guid fileId)
+    {
+        return await _context.OcrResults.FirstOrDefaultAsync(x => x.FileId == fileId);
+    }
+}
