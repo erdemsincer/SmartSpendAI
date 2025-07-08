1e2f6e75a52921cd6747da99fbe678d7228df1a2
Erdem <erdemsincer123@icloud.com>
2025-07-08 14:34:12 +0300

    Add DTO for receipt upload request

---
+++ src/Services/UploadService/UploadService.Application/DTOs/UploadReceiptRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace UploadService.Application.DTOs;
+
+public class UploadReceiptRequest
+{
+    public Guid UserId { get; set; }
+    public string FileName { get; set; } = null!;
+    public string FilePath { get; set; } = null!;
+    public int FileType { get; set; } // Enum olarak gelir (0 = Image, 1 = Pdf)
+}
