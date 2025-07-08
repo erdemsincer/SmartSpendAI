3bbcbcbd7bf6700a49dc0e154bf6cd25ecb74ee1
Erdem <erdemsincer123@icloud.com>
2025-07-08 20:58:14 +0300

    Add FileUploadedEvent to shared event definitions

---
+++ src/Services/OCRService/OCRService.Shared/Events/FileUploadedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace OCRService.Shared.Events;
+
+public class FileUploadedEvent
+{
+    public Guid FileId { get; set; }
+    public Guid UserId { get; set; }
+    public string FilePath { get; set; } = null!;
+}
+    
