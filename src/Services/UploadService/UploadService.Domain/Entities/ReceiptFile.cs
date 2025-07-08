813526a6a1374c9ce72f4bfc526b965e0b4c89e5
Erdem <erdemsincer123@icloud.com>
2025-07-08 14:34:18 +0300

    Add ReceiptFile entity to represent uploaded receipts

---
+++ src/Services/UploadService/UploadService.Domain/Entities/ReceiptFile.cs
@@ -0,0 +1,18 @@
+﻿using UploadService.Domain.Enums;
+
+namespace UploadService.Domain.Entities;
+
+public class ReceiptFile
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public Guid UserId { get; set; }  // Dosya kime ait
+
+    public string FileName { get; set; } = null!;
+    public string FilePath { get; set; } = null!;
+    public FileType FileType { get; set; }
+
+    public DateTime UploadedAt { get; set; } = DateTime.UtcNow;
+
+    public bool IsProcessed { get; set; } = false;  // OCR yapıldı mı?
+}
