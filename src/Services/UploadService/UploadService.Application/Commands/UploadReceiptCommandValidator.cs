f0830fc065e14627627866d14abeca5a7e50942e
Erdem <erdemsincer123@icloud.com>
2025-07-08 14:34:11 +0300

    Add FluentValidation for UploadReceiptCommand

---
+++ src/Services/UploadService/UploadService.Application/Commands/UploadReceiptCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace UploadService.Application.Commands;
+
+public class UploadReceiptCommandValidator : AbstractValidator<UploadReceiptCommand>
+{
+    public UploadReceiptCommandValidator()
+    {
+        RuleFor(x => x.FileName).NotEmpty();
+        RuleFor(x => x.FilePath).NotEmpty();
+        RuleFor(x => x.UserId).NotEmpty();
+        RuleFor(x => x.FileType).IsInEnum();
+    }
+}
