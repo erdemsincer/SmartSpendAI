b3b98df51d2b6145fbdfa20b98a595366eaef62a
Erdem <erdemsincer123@icloud.com>
2025-07-08 21:58:32 +0300

    Add OcrProcessedEvent to shared events

---
+++ src/Services/OCRService/OCRService.Shared/Events/OcrProcessedEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace OCRService.Shared.Events;
+
+public class OcrProcessedEvent
+{
+    public Guid FileId { get; set; }
+    public Guid UserId { get; set; }
+    public string RawText { get; set; } = null!;
+}
