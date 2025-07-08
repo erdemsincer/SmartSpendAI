b3a671590caef011c890f1b7d379daf786436990
Erdem <erdemsincer123@icloud.com>
2025-07-08 21:58:41 +0300

    Update UploadReceiptCommand to support OCR event

--- src/Services/UploadService/UploadService.Application/Commands/UploadReceiptCommand.cs
+++ src/Services/UploadService/UploadService.Application/Commands/UploadReceiptCommand.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using UploadService.Domain.Enums;
 
 namespace UploadService.Application.Commands;
 
@@ -7,5 +8,5 @@ public class UploadReceiptCommand : IRequest<Guid>  // Dönüş: File ID
     public Guid UserId { get; init; }
     public string FileName { get; init; } = null!;
     public string FilePath { get; init; } = null!;
-    public int FileType { get; init; }
+    public FileType FileType { get; init; }
 }
