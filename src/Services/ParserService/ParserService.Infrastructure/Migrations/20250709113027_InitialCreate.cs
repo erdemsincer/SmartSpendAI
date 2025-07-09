3723955ed1b5345460e6f0f8fe4580b7eb71b28d
Erdem <erdemsincer123@icloud.com>
2025-07-09 14:36:09 +0300

    Add initial EF migration for ParserService

---
+++ src/Services/ParserService/ParserService.Infrastructure/Migrations/20250709113027_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParserService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ParsedReceipts",
+                columns: table => new
+                {
+                    FileId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Merchant = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    TotalAmount = table.Column<decimal>(type: "numeric(10,2)", precision: 10, scale: 2, nullable: true),
+                    ParsedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParsedReceipts", x => x.FileId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ParsedReceipts");
+        }
+    }
+}
