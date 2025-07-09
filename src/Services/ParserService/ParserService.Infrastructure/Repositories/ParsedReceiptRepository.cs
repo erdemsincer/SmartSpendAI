67d657cdd255cdeda092ab6f524decde572495d3
Erdem <erdemsincer123@icloud.com>
2025-07-09 14:36:09 +0300

    Add ParsedReceiptRepository implementation

---
+++ src/Services/ParserService/ParserService.Infrastructure/Repositories/ParsedReceiptRepository.cs
@@ -0,0 +1,20 @@
+﻿using ParserService.Core.Entities;
+using ParserService.Core.Interfaces;
+
+namespace ParserService.Infrastructure.Repositories;
+
+public class ParsedReceiptRepository : IParsedReceiptRepository
+{
+    private readonly ParserDbContext _context;
+
+    public ParsedReceiptRepository(ParserDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(ParsedReceipt receipt)
+    {
+        _context.ParsedReceipts.Add(receipt);
+        await _context.SaveChangesAsync();
+    }
+}
