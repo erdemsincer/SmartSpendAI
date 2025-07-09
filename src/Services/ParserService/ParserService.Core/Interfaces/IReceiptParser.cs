c87c1777889262da2b1efd72277eb5e4f5fe5362
Erdem <erdemsincer123@icloud.com>
2025-07-09 14:13:19 +0300

    Add IReceiptParser interface for extensible parsing

---
+++ src/Services/ParserService/ParserService.Core/Interfaces/IReceiptParser.cs
@@ -0,0 +1,8 @@
+﻿using ParserService.Core.Entities;
+
+namespace ParserService.Core.Interfaces;
+
+public interface IReceiptParser
+{
+    ParsedReceipt Parse(Guid fileId, Guid userId, string rawText);
+}
