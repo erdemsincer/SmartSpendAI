edc40a7662d4407353cc665686dc446627ef8f6a
Erdem <erdemsincer123@icloud.com>
2025-07-09 14:13:19 +0300

    Add ParsedReceipt entity for parsed data structure

---
+++ src/Services/ParserService/ParserService.Core/Entities/ParsedReceipt.cs
@@ -0,0 +1,11 @@
+﻿namespace ParserService.Core.Entities;
+
+public class ParsedReceipt
+{
+    public Guid FileId { get; set; }
+    public Guid UserId { get; set; }
+
+    public string? Merchant { get; set; }
+    public decimal? TotalAmount { get; set; }
+    public DateTime ParsedAt { get; set; } = DateTime.UtcNow;
+}
