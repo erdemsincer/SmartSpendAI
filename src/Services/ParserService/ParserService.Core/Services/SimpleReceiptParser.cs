6fc38fecf2f23c674e40b4619b883d4ca5ae8d5e
Erdem <erdemsincer123@icloud.com>
2025-07-09 14:36:09 +0300

    Update SimpleReceiptParser to support parsed result structure

--- src/Services/ParserService/ParserService.Core/Services/SimpleReceiptParser.cs
+++ src/Services/ParserService/ParserService.Core/Services/SimpleReceiptParser.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
+using ParserService.Core.Entities;
+using ParserService.Core.Interfaces;
 
-namespace ParserService.Core.Services
+namespace ParserService.Core.Services;
+
+public class SimpleReceiptParser : IReceiptParser
 {
-    internal class SimpleReceiptParser
+    public ParsedReceipt Parse(Guid fileId, Guid userId, string rawText)
     {
+        // Basit örnek: en büyük sayı = toplam tutar
+        var totalMatch = Regex.Matches(rawText, @"\d+[\.,]?\d*")
+            .Select(m => decimal.TryParse(m.Value.Replace(',', '.'), out var val) ? val : 0)
+            .DefaultIfEmpty()
+            .Max();
+
+        var merchant = rawText.Split('\n').FirstOrDefault()?.Trim();
+
+        return new ParsedReceipt
+        {
+            FileId = fileId,
+            UserId = userId,
+            Merchant = merchant,
+            TotalAmount = totalMatch
+        };
     }
 }
