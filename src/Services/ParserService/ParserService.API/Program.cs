7629e155405a78cfece1a1a362643a2bcf7869bd
Erdem <erdemsincer123@icloud.com>
2025-07-09 14:13:16 +0300

    Configure ParserService API startup

---
+++ src/Services/ParserService/ParserService.API/Program.cs
@@ -0,0 +1,31 @@
+using ParserService.API.Services;
+using ParserService.Core.Interfaces;
+using ParserService.Core.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+builder.Services.AddHostedService<ParserWorker>();
+builder.Services.AddSingleton<IReceiptParser, SimpleReceiptParser>();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
