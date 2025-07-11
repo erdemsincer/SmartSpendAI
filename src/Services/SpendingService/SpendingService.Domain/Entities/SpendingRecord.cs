c7e5caf4008370459c53e1c577ecc751db9fa63c
Erdem <erdemsincer123@icloud.com>
2025-07-11 16:49:33 +0300

    Add SpendingRecord entity

---
+++ src/Services/SpendingService/SpendingService.Domain/Entities/SpendingRecord.cs
@@ -0,0 +1,11 @@
+﻿namespace SpendingService.Domain.Entities;
+
+public class SpendingRecord
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid UserId { get; set; }
+    public Guid ReceiptId { get; set; }
+    public string Category { get; set; } = string.Empty;
+    public decimal Amount { get; set; }
+    public DateTime Timestamp { get; set; }
+}
