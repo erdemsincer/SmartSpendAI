129a4f81fc3c3452af18f8e3808f809d7e0ecc6a
Erdem <erdemsincer123@icloud.com>
2025-07-11 16:49:41 +0300

    Add SpendingRepository implementation

---
+++ src/Services/SpendingService/SpendingService.Infrastructure/Repositories/SpendingRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpendingService.Domain.Entities;
+using SpendingService.Domain.Interfaces;
+using SpendingService.Infrastructure.Db;
+
+namespace SpendingService.Infrastructure.Repositories;
+
+public class SpendingRepository : ISpendingRepository
+{
+    private readonly AppDbContext _db;
+
+    public SpendingRepository(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task AddAsync(SpendingRecord record)
+    {
+        _db.SpendingRecords.Add(record);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task<List<SpendingRecord>> GetByUserAsync(Guid userId)
+    {
+        return await _db.SpendingRecords
+            .Where(x => x.UserId == userId)
+            .ToListAsync();
+    }
+}
