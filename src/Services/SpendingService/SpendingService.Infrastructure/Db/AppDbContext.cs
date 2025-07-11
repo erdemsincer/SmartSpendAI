c82e17adb709917b5124c995746f6801df2cfebd
Erdem <erdemsincer123@icloud.com>
2025-07-11 16:49:41 +0300

    Add AppDbContext for EF in SpendingService

---
+++ src/Services/SpendingService/SpendingService.Infrastructure/Db/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpendingService.Domain.Entities;
+using System.Collections.Generic;
+
+namespace SpendingService.Infrastructure.Db;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<SpendingRecord> SpendingRecords => Set<SpendingRecord>();
+}
