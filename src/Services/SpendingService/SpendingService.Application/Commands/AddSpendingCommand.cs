c6e4bc268d3a1ba1a1c0cd23d333eb0c8eca74c6
Erdem <erdemsincer123@icloud.com>
2025-07-11 16:49:25 +0300

    Add AddSpendingCommand for CQRS structure

---
+++ src/Services/SpendingService/SpendingService.Application/Commands/AddSpendingCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace SpendingService.Application.Commands;
+
+public class AddSpendingCommand : IRequest<Unit>
+{
+    public Guid UserId { get; set; }
+    public Guid ReceiptId { get; set; }
+    public string Category { get; set; } = default!;
+    public decimal Amount { get; set; }
+    public DateTime Timestamp { get; set; }
+}
