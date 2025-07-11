a7519d11440d1e2baa176d0609fa2e71c8c0d291
Erdem <erdemsincer123@icloud.com>
2025-07-11 16:49:33 +0300

    Add ISpendingRepository interface

---
+++ src/Services/SpendingService/SpendingService.Domain/Interfaces/ISpendingRepository.cs
@@ -0,0 +1,9 @@
+﻿using SpendingService.Domain.Entities;
+
+namespace SpendingService.Domain.Interfaces;
+
+public interface ISpendingRepository
+{
+    Task AddAsync(SpendingRecord record);
+    Task<List<SpendingRecord>> GetByUserAsync(Guid userId);
+}
